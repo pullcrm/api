4648c8a670b8c7c6987dee0cf9f4470125d12846
tsarr <bogdan.tsarik@gmailcom>
2020-12-27 11:53:32 +0200

    Move sms login and password to `.env`

--- app/providers/smsc/index.js
+++ app/providers/smsc/index.js
@@ -1,8 +1,11 @@
 import SMSClient from './client.js'
 
+const SMS_CLIENT_LOGIN = process.env.SMS_CLIENT_LOGIN
+const SMS_CLIENT_PASSWORD = process.env.SMS_CLIENT_PASSWORD
+
 export const globalSMS = new SMSClient({
-  login: 'tsarr',
-  password: 'Xo4Ar2g8'
+  login: SMS_CLIENT_LOGIN,
+  password: SMS_CLIENT_PASSWORD
 })
 
 export const privateSMS = (login, password) => new SMSClient(login, password)
