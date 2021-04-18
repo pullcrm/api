9a046fa77cf1df0f540ec21c6d3472d46e7fbe07
uramen <uramen12@gmail.com>
2021-04-18 12:35:24 +0300

    add todo

--- app/modules/timework/routes/timework.public.js
+++ app/modules/timework/routes/timework.public.js
@@ -2,6 +2,8 @@ import {Router} from 'express'
 import TomeWorkController from '../timework.controller'
 
 const router = Router()
+
+//TODO in future, it can be attached to the 'company' entity
 router.get('/:companyId', TomeWorkController.publicFindTimeWork)
 
 export default router
