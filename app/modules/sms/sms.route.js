70b5a6bc3a22a6f7b57619927fc5620c9868872b
uramen <uramen12@gmail.com>
2021-06-26 17:28:43 +0300

    clear

--- app/modules/sms/sms.route.js
+++ app/modules/sms/sms.route.js
@@ -7,10 +7,8 @@ const router = Router()
 
 router.get('/balance', auth([ADMIN, SPECIALIST, MANAGER]), SMSController.balance)
 router.post('/status', auth([ADMIN, MANAGER]), SMSController.status)
-router.post('/destroy', auth([ADMIN, MANAGER]), SMSController.destroy)
 router.post('/settings', auth([ADMIN, MANAGER]), SMSController.addSettings)
 router.put('/settings', auth([ADMIN, MANAGER]), SMSController.updateSettings)
 router.delete('/settings', auth([ADMIN, MANAGER]), SMSController.deleteSettings)
-// router.post('/send', SMSController.sendSms)
 
 export default router
