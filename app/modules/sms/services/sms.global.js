b17295bdf4e965bee2b71969cb1fabccc3b84416
uramen <uramen12@gmail.com>
2021-07-19 10:41:03 +0300

    expand addUAFormat halper

--- app/modules/sms/services/sms.global.js
+++ app/modules/sms/services/sms.global.js
@@ -1,11 +1,12 @@
 import globalSMS from '../../../providers/epochta/globalClient'
+import {addUAFormat} from '../../../utils/phone'
 
 export default {
   send: async ({phone, message}) => {
     return globalSMS.sendSMS({
       sender: process.env.SMS_COMPANY_NAME,
       text: message,
-      phone
+      phone: addUAFormat(phone)
     })
   },
 }
