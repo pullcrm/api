5922ab6a82a98681d0cb0b90c88ca5bac7b1dafe
tsarr <bogdan.tsarik@gmail.com>
2021-08-10 12:31:06 +0300

    Revert WEB-14

--- app/modules/appointments/appointment.controller.js
+++ app/modules/appointments/appointment.controller.js
@@ -89,7 +89,7 @@ export default {
         source: joi.string().valid(WIDGET, ADMIN_PANEL),
       }))
 
-      await TimeOffService.checkForAvailableTime(formattedData)
+      // await TimeOffService.checkForAvailableTime(formattedData)
       const appointment = await AppointmentService.create(formattedData)
       await SMSPrivateService.sendAfterAppointmentCreate({...formattedData, appointmentId: appointment.id})
 
@@ -314,7 +314,7 @@ export default {
         hasCreationSMS: joi.boolean(),
       }))
 
-      await TimeOffService.checkForAvailableTime(formattedData)
+      // await TimeOffService.checkForAvailableTime(formattedData)
       const appointment = await AppointmentService.create(formattedData)
       await SMSPrivateService.sendAfterAppointmentCreate({...formattedData, appointmentId: appointment.id})
 
