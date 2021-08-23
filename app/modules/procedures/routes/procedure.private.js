159ce7d48bfde2d0818132785243ab2fda2e1872
tsarr <bogdan.tsarik@gmail.com>
2021-08-23 21:04:26 +0300

    Minor fix

--- app/modules/procedures/routes/procedure.private.js
+++ app/modules/procedures/routes/procedure.private.js
@@ -7,7 +7,7 @@ const router = Router()
 
 router.get('/', auth([ADMIN, MANAGER, SPECIALIST]), ProcedureController.index)
 router.post('/', auth([ADMIN, MANAGER]), ProcedureController.create)
-router.put('/bulk', auth(ADMIN, MANAGER), ProcedureController.bulkUpdate)
+router.put('/bulk', auth([ADMIN, MANAGER]), ProcedureController.bulkUpdate)
 router.put('/:id', auth([ADMIN, MANAGER]), ProcedureController.update)
 router.delete('/:id', auth([ADMIN, MANAGER]), ProcedureController.destroy)
 
