ccc030a299f9585acfca973bad488748526ed388
uramen <uramen12@gmail.com>
2022-09-10 15:19:59 +0300

    WEB-144: Add edit/destroy and startDate param

--- app/modules/timework/timework.service.js
+++ app/modules/timework/timework.service.js
@@ -1,3 +1,7 @@
+import {isEmpty} from 'lodash'
+import {Op} from 'sequelize'
+import ApiException from '../../exceptions/api'
+import { addDayToDate } from '../../utils/time'
 import TimeWorkModel from './models/companyTimework'
 import SpecialistTimeWorkModel from './models/specialistTimework'
 
@@ -16,11 +20,71 @@ export default {
     return timeWork
   },
 
+  checkIfExist: async ({timeWork, specialistId}) => {
+    //TODO: NEED TO FINISH
+    const specialistTimeWork = await SpecialistTimeWorkModel.findAll({
+      where: {
+        specialistId,
+        [Op.or]: [
+          {
+            startDateTime: {
+              [Op.gt]: timeWork.map(T => T.startDateTime),
+              [Op.lt]: timeWork.map(T => T.endDateTime),
+            },
+          },
+          {
+            endDateTime: {
+              [Op.gt]: timeWork.map(T => T.startDateTime),
+              [Op.lt]: timeWork.map(T => T.endDateTime),
+            },
+          },
+        ],
+      },
+      raw: true,
+    })
+
+    console.log(specialistTimeWork)
+
+    if (!isEmpty(specialistTimeWork)) {
+      throw new ApiException(400, "The time for this day is already set")
+    }
+
+    return specialistTimeWork
+  },
+
   bulkSpecialistTimeWorkCreate: async ({timeWork, specialistId}) => {
     return SpecialistTimeWorkModel.bulkCreate(timeWork.map(T => ({...T, specialistId})))
   },
 
-  getSpecialistTimeWork: async ({specialistId}) => {
-    return SpecialistTimeWorkModel.findAll({where: {specialistId}})
+  specialistTimeWorkUpdate: async (data, {timeWorkId}) => {
+    const specilistWorkTime = await SpecialistTimeWorkModel.findOne({where: {id: timeWorkId}})
+    return specilistWorkTime.update(data) 
+  },
+
+  specialistTimeWorkDestroy: async ({timeWorkId}) => {
+    const specilistWorkTime = await SpecialistTimeWorkModel.findOne({where: {id: timeWorkId}})
+    return specilistWorkTime.destroy({cascade: true})
+  },
+
+  getSpecialistTimeWork: async ({specialistId, startDate}) => {
+    const where = {specialistId}
+    const endDate = addDayToDate(startDate)
+
+    if(startDate) {
+      where[Op.or] = [
+        {
+          startDateTime: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+        {
+          endDateTime: {
+            [Op.between]: [startDate, endDate],
+          },
+        },
+      ]
+    }
+  
+    return SpecialistTimeWorkModel.findAll({where})
   }
 }
