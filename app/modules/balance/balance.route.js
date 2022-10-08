20682ed063aa73e6c14f2fdc98a065c890973745
uramen <uramen12@gmail.com>
2022-10-08 22:23:09 +0300

    expand getBalance route

--- app/modules/balance/balance.route.js
+++ app/modules/balance/balance.route.js
@@ -1,11 +1,11 @@
 import {Router} from 'express'
 import BalanceController from './balance.controller'
 import auth from '../../middlewares/auth'
-import {ADMIN, MANAGER} from '../../constants/roles'
+import {ADMIN, MANAGER, SPECIALIST} from '../../constants/roles'
 
 const router = Router()
 
-router.get('/', auth([ADMIN, MANAGER]), BalanceController.getBalance)
+router.get('/', auth([ADMIN, MANAGER, SPECIALIST]), BalanceController.getBalance)
 router.get('/history', auth([ADMIN, MANAGER]), BalanceController.getBalanceHistory)
 router.post('/checkout', auth([ADMIN, MANAGER]), BalanceController.checkout)
 router.post('/redirectUrl', BalanceController.redirectUrl)
